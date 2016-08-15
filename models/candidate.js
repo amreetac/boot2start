8f107c2ed222936ee2d561e4973417991bc8e3c0
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2016-08-14 23:43:11 -0400

    non-unique emails

--- models/candidate.js
+++ models/candidate.js
@@ -14,7 +14,6 @@ module.exports = function (sequelize, DataTypes) {
     },
     email: {
       type: DataTypes.STRING, // textfield
-      unique: true,
       allowNull: false
     },
     bootcampCourse: {
