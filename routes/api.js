3bdd0c6352a4cdf77e81b77bbdc6c8c5b3bc852a
Sudarshana <sudchavan@gmail.com>
2016-08-17 19:59:43 -0400

    Added bcrypt

--- routes/api.js
+++ routes/api.js
@@ -79,24 +79,52 @@ module.exports = function(app) {
         break;
 
       // CREATE USER
-      case 'user':
-        console.log('user route');
+      case 'user-signup':
+        console.log('user-signup route');
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(req.body.password, salt);
+
+        //creating a user from the email and password provided
         db.User.create({
-
-          email: body.email,
-          password: body.password
-
-        }).then(function() {
-          db.User.findAll({}).then(function(users) {
-            console.log(users);
-
-            res.json({
-              success: true,
-              users: users
-            });
-          });
-        });
-        break;
+          email: req.body.email,
+          password: hash
+         //sending the newly created user to the client
+                     }).then(function(dbUser) {
+                  res.json(dbUser.dataValues);
+           //if there are any errors creating our user, we will gracefully catch the error send the error to the client instead of throwing it (which would crash our server)
+         }).catch(function(err) {
+          res.json({message: err.message});
+         });
+         break;
+     // USER Signin
+      case 'user-signin':
+            console.log('user-signin route');
+            //looking for one user whos password has the email and password submitted
+         db.User.findOne({
+         where: {
+         email: req.body.email
+                }
+                        }).then(function(dbUser) {
+         //if no user is found, we'll send back a message saying so
+        if (!dbUser) {
+           res.json({
+              message: "User not found"
+                   });
+        } else if (bcrypt.compareSync(req.body.password, dbUser.password)) {
+        //otherwise we'll send back the user
+         res.json(dbUser.dataValues);
+        } else {
+        //if the password is invalid, we'll let the user know
+       res.json({
+             message: "Invalid Password"
+               });
+              }
+        //we gracefully handle any errors with our catch
+      }).catch(function(err) {
+             res.json(err);
+      });
+      );
+      break;
 
       // ROUTE NOT FOUND
       default:
