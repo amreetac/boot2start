a452d20e3179346d93f97382834fabf7bb9d20b8
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2016-08-18 23:19:03 -0400

    candidate picture and upload functionality

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,8 @@
 module.exports = function(app) {
+  const express = require('express');
+  const aws = require('aws-sdk');
+  const S3_BUCKET = 'boot2start'
+
   // GET landing page
   // ==============================================================================
   app.get('/', function(req, res) {
@@ -6,7 +10,6 @@ module.exports = function(app) {
     res.render('landing');
   });
 
-  // GET all bootcamps, candidates or startups
   // ==============================================================================
   app.get('/:route', function(req, res) {
     let route = req.params.route;
@@ -55,6 +58,33 @@ module.exports = function(app) {
         console.log('login route');
         res.render('login');
         break;
+
+      case 'sign-s3':
+        const s3 = new aws.S3();
+        const fileName = req.query['file-name'];
+        const fileType = req.query['file-type'];
+        const s3Params = {
+          Bucket: S3_BUCKET,
+          Key: fileName,
+          Expires: 60,
+          ContentType: fileType,
+          ACL: 'public-read'
+        };
+
+        s3.getSignedUrl('putObject', s3Params, (err, data) => {
+          if(err){
+            console.log(err);
+            return res.end();
+          }
+          console.log(data);
+          const returnData = {
+            signedRequest: data,
+            url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+          };
+          res.write(JSON.stringify(returnData));
+          res.end();
+        });
+        break;
       default:
         console.log('route not found:', route);
         res.json({
@@ -66,7 +96,6 @@ module.exports = function(app) {
 
   });
 
-  // GET bootcamp, startup or candidate
   // ==============================================================================
   app.get('/:route/:id', function(req, res) {
     let route = req.params.route;
