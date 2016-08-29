339318adccee96f578130168d6c78f1b3a938022
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2016-08-29 17:57:23 -0400

    relocate upload & login JS files'

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -7,17 +7,3 @@ console.log(data);       if (data.id){
 document.getElementById('id01').style.display='block'       }     });
 
 })
-
-
-// Get the modal
-// var modal = document.getElementById('id01');
-// $(document).click(function(){
-//   modal.style.display = "none";
-// });
-
-// // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-   if (event.target == modal) {
-       modal.style.display = "none";
-   }
-}
